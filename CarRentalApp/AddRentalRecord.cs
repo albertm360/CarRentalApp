c05943a1ccfae887fd8caa012ce05df534b47e7a
albertm360 <albertmorla@mac.com>
2023-04-02 22:58:46 +0200

    Updated app with connection to SQL Server

---
+++ CarRentalApp/AddRentalRecord.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarRentalApp
+{
+    public partial class AddRentalRecord : System.Windows.Forms.Form
+    {
+        // Every entity represented in the DB Diagram
+        private readonly CarRentalEntities carRentalEntities;
+        public AddRentalRecord()
+        {
+            InitializeComponent();
+            // Initialize a new instance of CarRentalEntities:
+            carRentalEntities = new CarRentalEntities();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string customerName = tbCustomerName.Text;
+                DateTime dateRented = dtDateRented.Value;
+                DateTime dateReturned = dtDateReturned.Value;
+                double cost = Convert.ToDouble(tbCost.Text);
+                var carType = cbCarType.Text;
+                var isValid = true;
+                var errorMessage = string.Empty;
+
+                if (string.IsNullOrWhiteSpace(customerName) || string.IsNullOrWhiteSpace(carType))
+                {
+                    isValid = false;
+                    errorMessage += "Error: Please, enter missing data.\n\r";
+                }
+
+                if (dateRented > dateReturned)
+                {
+                    isValid = false;
+                    errorMessage += "Error: Illegal date selection.\n\r";
+                }
+
+                // if(isValid == true)
+                if (isValid)
+                {
+                    // Instantiate a new object of the CarRentalRecord table:
+                    var rentalRecord = new CarRentalRecord();
+                    rentalRecord.CustomerName = customerName;
+                    // You can also do implicit casts to variables to convert to another type.
+                    // Instead of:
+                    // rentalRecord.Cost = Convert.ToDecimal(cost);
+                    rentalRecord.Cost = (decimal)cost;
+                    rentalRecord.DateRented = dateRented;
+                    rentalRecord.DateReturned = dateReturned;
+                    rentalRecord.TypeOfCarId = (int)cbCarType.SelectedValue;
+                    
+                    // Adds the content of rentalRecord to the database:
+                    carRentalEntities.CarRentalRecords.Add(rentalRecord);
+                    // But nothing gets registered until you save the changes:
+                    carRentalEntities.SaveChanges();
+
+                    MessageBox.Show($"Customer name: {customerName}\n\r" +
+                        $"Date rented: {dateRented}\n\r" +
+                        $"Date returned: {dateReturned}\n\r" +
+                        $"Type of car: {carType}\n\r" +
+                        $"Cost: {cost}\n\r\n\r" +
+                        $"Thank you for your bussiness!");
+
+                }
+                else
+                {
+                    MessageBox.Show(errorMessage);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                // throw; // 
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Native C# but the library is called LINQ:
+            // select * from TypesOfCars
+            var cars = carRentalEntities.TypesOfCars.ToList();
+            // DisplayMember: display the name:
+            cbCarType.DisplayMember = "name";
+            // ValueMember: store the id:
+            cbCarType.ValueMember = "id";
+            // DataSource: the source for the combo box comes from cars:
+            cbCarType.DataSource = cars;
+        }
+
+        private void btn_mainWindow_Click(object sender, EventArgs e)
+        {
+            // Instantiate MainWindow Form:
+            MainWindow mainWindow = new MainWindow();
+            // Call the MainWindows form when the button is pressed:
+            mainWindow.Show();
+        }
+    }
+}
---
+++ CarRentalApp/MainWindow.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace CarRentalApp
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_title = new System.Windows.Forms.Label();
+            this.btnCarRentalSystem = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_title
+            // 
+            this.lbl_title.AutoSize = true;
+            this.lbl_title.Font = new System.Drawing.Font("Cascadia Code SemiBold", 20F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_title.Location = new System.Drawing.Point(273, 9);
+            this.lbl_title.Name = "lbl_title";
+            this.lbl_title.Size = new System.Drawing.Size(240, 45);
+            this.lbl_title.TabIndex = 1;
+            this.lbl_title.Text = "Main Window";
+            this.lbl_title.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnCarRentalSystem
+            // 
+            this.btnCarRentalSystem.Font = new System.Drawing.Font("Cascadia Code SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCarRentalSystem.Location = new System.Drawing.Point(275, 199);
+            this.btnCarRentalSystem.Name = "btnCarRentalSystem";
+            this.btnCarRentalSystem.Size = new System.Drawing.Size(250, 53);
+            this.btnCarRentalSystem.TabIndex = 10;
+            this.btnCarRentalSystem.Text = "Add Rental Record";
+            this.btnCarRentalSystem.UseVisualStyleBackColor = true;
+            this.btnCarRentalSystem.Click += new System.EventHandler(this.btnCarRentalSystem_Click);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCarRentalSystem);
+            this.Controls.Add(this.lbl_title);
+            this.Name = "MainWindow";
+            this.Text = "Main Window";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_title;
+        private System.Windows.Forms.Button btnCarRentalSystem;
+    }
+}
